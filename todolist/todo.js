ee8d512f55c303950993273ba7bbaffb20203124
Ludovico Pio Gnerre <ellpgi@gmail.com>
2023-01-19 22:47:27 +0100

    ok

--- todolist/todo.js
+++ todolist/todo.js
@@ -1,118 +1,82 @@
-let btn = document.getElementById ("btn")
-let list = document.getElementById ("list")
-let input = document.getElementById ("input")
-let form = document.getElementById ("form")
-const deletediv = document.getElementById("delete-all")
-
-
-
-
-
+let btn = document.getElementById("btn");
+let list = document.getElementById("list");
+let input = document.getElementById("input");
+let form = document.getElementById("form");
+const deletediv = document.getElementById("delete-all");
 
 form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    
-    let inputvalue = input.value
+  e.preventDefault();
 
-    if (!inputvalue){
-        alert("Inserisci un task")
-    }
-    else {
-    var task= document.createElement ("li")
-    let tasktext= document.createTextNode (inputvalue)
+  let inputvalue = input.value;
 
-    var editbutton = document.createElement ("button")
+  if (!inputvalue) {
+    alert("Inserisci un task");
+  } else {
+    var task = document.createElement("li");
+    let tasktext = document.createTextNode(inputvalue);
+
+    var editbutton = document.createElement("button");
     editbutton.className = "edit";
-    editbutton.setAttribute("type", "button")
-    editbutton.innerHTML= `<i class="fa fa-pencil-square-o" aria-hidden="true"></i>`
+    editbutton.setAttribute("type", "button");
+    editbutton.innerHTML = `<i class="fa fa-pencil-square-o" aria-hidden="true"></i>`;
 
-    var deletebutton = document.createElement ("button")
+    var deletebutton = document.createElement("button");
     deletebutton.className = "delete";
-    deletebutton.setAttribute("type", "button")
-    deletebutton.innerHTML = `<i class="fa fa-trash" aria-hidden="true"></i>`
+    deletebutton.setAttribute("type", "button");
+    deletebutton.innerHTML = `<i class="fa fa-trash" aria-hidden="true"></i>`;
 
+    task.appendChild(tasktext);
+    list.appendChild(task);
 
-    task.appendChild(tasktext)
-    list.appendChild(task)
+    task.appendChild(editbutton);
 
-    task.appendChild(editbutton)
-    
-    task.appendChild(deletebutton)
+    task.appendChild(deletebutton);
 
-    input.value= ""    
-    }
+    input.value = "";
+  }
 
-    deletediv.style.display="block"
-              
-      
-        
-   
+  deletediv.style.display = "block";
 
-    deletediv.addEventListener("click", () => {
-        task.remove()
-        deletediv.style.display="none"
+  deletediv.addEventListener("click", () => {
+    task.remove();
+    deletediv.style.display = "none";
+  });
 
-    })
+  deletebutton.addEventListener("click", () => {
+    task.remove();
+  });
 
-    deletebutton.addEventListener("click", () => {
-       task.remove()
-       
-    })
+  editbutton.addEventListener("click", () => {
+    let newli = document.createElement("li");
 
+    let newinput = document.createElement("input");
+    newinput.className = "newinput";
+    let savebutton = document.createElement("button");
 
-    editbutton.addEventListener("click", ()  => {
-           
-           
-        
-        let newli = document.createElement ("li")
-        
+    savebutton.setAttribute("type", "button");
+    savebutton.className = "save";
+    savebutton.innerHTML = `<i class="fa fa-floppy-o " aria-hidden="true"></i> `;
 
-        let newinput= document.createElement ("input")
-        newinput.className = "newinput"
-        let savebutton = document.createElement ("button")
-        
-        savebutton.setAttribute ("type", "button")
-        savebutton.className = "save"
-        savebutton.innerHTML= `<i class="fa fa-floppy-o " aria-hidden="true"></i> `
+    newli.appendChild(newinput);
+    newli.appendChild(savebutton);
 
-       
-        
-        newli.appendChild (newinput)
-        newli.appendChild (savebutton)
-    
-        
-        list.replaceChild(newli, task);
+    list.replaceChild(newli, task);
 
-        savebutton.addEventListener("click", () =>{
-            newinputvalue = newinput.value
+    savebutton.addEventListener("click", () => {
+      newinputvalue = newinput.value;
 
-            if (!newinputvalue){
-                alert("Insert an edit")
-            }
-            
-            else{
-             task = document.createElement ("li")
-             tasktext= document.createTextNode (newinputvalue)
+      if (!newinputvalue) {
+        alert("Insert an edit");
+      } else {
+        task = document.createElement("li");
+        tasktext = document.createTextNode(newinputvalue);
 
-            task.appendChild(tasktext)
-            task.appendChild(editbutton)
-            task.appendChild(deletebutton)
+        task.appendChild(tasktext);
+        task.appendChild(editbutton);
+        task.appendChild(deletebutton);
 
-            list.replaceChild(task, newli)
-
-            
-            
-             
-            }
-        })
-
-        
-        
-    })
-
-   
-        
-    
-  
-})
-
+        list.replaceChild(task, newli);
+      }
+    });
+  });
+});
